267593cbcbb03385b9a07ae893993dbc5c76a6f9
arahman225 <upworkabdurrahman@gmail.com>
2025-09-27 22:10:37 +0600

    set up and installed neccessary npm package

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express"
+import cors from "cors"
+import compression from "compression"
+
+
+const app = express()
+
+// Middleware------------------
+app.use(cors())
+app.use(compression())
+app.use(express.json())
+
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}))
+
+
+// Default route testing
+
+app.get('/',(req:Request, res:Response)=>{
+    res.send("Server is running")
+})
